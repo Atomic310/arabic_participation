606edd413b0022df501c09a6ac5a33292ab331e9
joshuakim135 <joshuakim135@gmail.com>
2022-09-20 18:34:06 -0500

    integration testing init

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -4,14 +4,20 @@ require 'rails_helper'
 RSpec.describe 'Creating a member', type: :feature do
     scenario 'valid inputs' do
         visit new_member_path
-        fill_in 'uin', with: 928006659
-        fill_in 'first_name', with: 'Harry'
-        fill_in 'last_name', with: 'Potter'
-        fill_in 'classification', with: 'Senior'
-        fill_in 'major', with: 'Gender Studies'
-        fill_in 'email', with: 'harrypotter@hotmail.com'
-        click_on 'Create Members'
-        visit member_path
-        expect(page).to have_content('harry potter')
+        fill_in 'member_uin', with: 928006659
+        fill_in 'member_first_name', with: 'Harry'
+        fill_in 'member_last_name', with: 'Potter'
+        fill_in 'member_classification', with: 'Senior'
+        fill_in 'member_email', with: 'harrypotter@hotmail.com'
+        fill_in 'member_arabic_lvl', with: 'Beginner'
+        click_on 'Create Member'
+        
+        visit members_path
+        expect(page).to have_content(928006659)
+        expect(page).to have_content('Harry')
+        expect(page).to have_content('Potter')
+        expect(page).to have_content('Senior')
+        expect(page).to have_content('harrypotter@hotmail.com')
+        expect(page).to have_content('Beginner')
     end
 end
