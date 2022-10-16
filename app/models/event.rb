f7648a1ae55d207243460ac62835fb8600abbc12
root <root@localhost>
2022-10-16 12:20:44 +0000

    recreated review entity and linked it to events/members

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ApplicationRecord
+    has_many :members, through: :reviews
+    has_many :reviews
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,6 @@
 class Member < ApplicationRecord
+    has_many :events, through: :reviews
+    has_many :reviews 
+    
     validates :uin, :first_name, :last_name, :email, :classification, :arabic_lvl, :payment_status, presence: true
 end
