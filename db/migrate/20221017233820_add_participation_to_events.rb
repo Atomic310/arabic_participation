78037324ac16d44e18acad6ab84d452473559166
atomic310 <atomic310@tamu.edu>
2022-10-17 20:16:15 -0500

    added participation

--- db/migrate/20221017233820_add_participation_to_events.rb
+++
@@ -1,5 +0,0 @@
-class AddParticipationToEvents < ActiveRecord::Migration[6.1]
-  def change
-    add_column :events, :participation, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_17_233820) do
+ActiveRecord::Schema.define(version: 2022_10_16_123646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 2022_10_17_233820) do
     t.integer "event_type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "participation"
   end
 
   create_table "logs", force: :cascade do |t|
