611890a063272402337fd4d0b3786a872ca4d86d
joshuakim135 <joshuakim135@gmail.com>
2022-10-18 15:55:35 -0500

    merge fix

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,8 @@
 class Event < ApplicationRecord
-<<<<<<< HEAD
     has_many :log
     has_many: review
 
     validates :event_id, :date, :title, :description, :location, :price, :type, presence: true
-=======
     has_many :members, through: :reviews 
     has_many :reviews
->>>>>>> main
 end
