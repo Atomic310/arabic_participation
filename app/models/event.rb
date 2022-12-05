ec72c440349b6df96ff9fb84144680eede154874
Thachdo1219 <thachdo1219@tamu.edu>
2022-12-05 06:50:22 +0000

    review fix

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,3 @@
 class Event < ApplicationRecord
-    has_many :members, through: :reviews 
     has_many :reviews, dependent: :destroy_async
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,3 @@
 class Review < ApplicationRecord
-    belongs_to :member 
     belongs_to :event
 end
