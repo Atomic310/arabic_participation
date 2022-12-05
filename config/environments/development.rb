9391a9c8995c136e53113731d3d7b67c3678c682
root <root@localhost>
2022-12-05 19:01:50 +0000

    enabled password reset - changed host address

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,6 +75,6 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   #for DEVISE GEM, this is for local testing
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://arabic-attendance-tracker.herokuapp.com/' }
 
 end
